3d738540b79a93d75a00e3f25c615885bade22c3
chen00054 <73772128+chen00054@users.noreply.github.com>
2021-10-15 10:46:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,4 @@ sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_genera
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
 sed -i 's/OpenWrt/iPhone8/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/CMCC-A281/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
